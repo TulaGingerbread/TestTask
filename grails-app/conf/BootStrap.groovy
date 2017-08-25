7d1b6c68c90818c1c44b6c47c80467d0b72e7790
TulaGingerbread <tulagingerbread@gmail.com>
2017-08-25 17:38:57 +0500

    Add dummy data for colleges Change default page to list of students

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import testtask.College
+
 class BootStrap {
 
     def init = { servletContext ->
+        new College(name: 'College #1').save()
+        new College(name: 'College #2').save()
+        new College(name: 'College #3').save()
+        new College(name: 'College #4').save()
     }
     def destroy = {
     }
